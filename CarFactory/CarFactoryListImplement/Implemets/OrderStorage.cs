91cc7587d3c9c50fe2eed68dedcfc46a795b2a82
ktrepfk <ktrepfk@mail.ru>
2022-03-13 12:20:41 +0400

    (

--- CarFactory/CarFactoryListImplement/Implemets/OrderStorage.cs
+++ CarFactory/CarFactoryListImplement/Implemets/OrderStorage.cs
@@ -35,7 +35,7 @@ namespace CarFactoryListImplement.Implemets
             var result = new List<OrderViewModel>();
             foreach(var order in source.Orders)
             {
-                if (order.CarId == model.CarId)
+                if (order.CarId == model.CarId || (order.DateCreate >= model.DateFrom && order.DateCreate <= model.DateTo))
                 {
                     result.Add(CreateModel(order));
                 }
